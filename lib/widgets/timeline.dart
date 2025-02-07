08c136cdde9ed053533405a71c6f388e6cad298f
Mike Sheldon <mike@mikeasoft.com>
2025-02-07 17:26:35 +0000

    Only display statuses containing audio

--- lib/widgets/timeline.dart
+++ lib/widgets/timeline.dart
@@ -109,9 +109,19 @@ class _TimelineState extends State<Timeline> {
     updateTimeline(false);
   }
 
+  bool hasAudio(api.Status status) {
+    for (var media in status.reblog != null
+        ? status.reblog!.mediaAttachments
+        : status.mediaAttachments) {
+      if (media.type == api.MediaAttachmentType.audio) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(widget.statuses);
     return Scaffold(
         body: SmartRefresher(
       enablePullDown: true,
@@ -124,153 +134,165 @@ class _TimelineState extends State<Timeline> {
               child: SizedBox(
                   height: 64, width: 64, child: CircularProgressIndicator()))
           : ListView.builder(
-              itemBuilder: (c, i) => Card(
-                  clipBehavior: Clip.hardEdge,
-                  margin: const EdgeInsets.only(
-                      left: 10, right: 10, top: 10, bottom: 0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(children: [
-                        Row(
-                          children: [
-                            Stack(clipBehavior: Clip.none, children: [
-                              ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image(
-                                      image: NetworkImage(
-                                          widget.statuses[i].reblog != null
-                                              ? widget.statuses[i].reblog!
-                                                  .account.avatar
-                                              : widget
-                                                  .statuses[i].account.avatar),
-                                      width: 32,
-                                      height: 32)),
-                              Visibility(
-                                  visible: widget.statuses[i].reblog != null,
-                                  child: Positioned(
-                                      left: 16,
-                                      top: 18,
-                                      child: ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(5),
-                                          child: Image(
-                                              image: NetworkImage(widget
-                                                  .statuses[i].account.avatar),
-                                              width: 18,
-                                              height: 18)))),
-                            ]),
-                            const SizedBox(width: 5),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+              itemBuilder: (c, i) => hasAudio(widget.statuses[i])
+                  ? Card(
+                      clipBehavior: Clip.hardEdge,
+                      margin: const EdgeInsets.only(
+                          left: 10, right: 10, top: 10, bottom: 0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(children: [
+                            Row(
                               children: [
-                                Visibility(
-                                    visible: widget.statuses[i].reblog != null,
-                                    child: Text(
-                                      widget.statuses[i].account.displayName !=
-                                              ""
-                                          ? "${widget.statuses[i].account.displayName} boosted"
-                                          : "${widget.statuses[i].account.username} boosted",
+                                Stack(clipBehavior: Clip.none, children: [
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image(
+                                          image: NetworkImage(
+                                              widget.statuses[i].reblog != null
+                                                  ? widget.statuses[i].reblog!
+                                                      .account.avatar
+                                                  : widget.statuses[i].account
+                                                      .avatar),
+                                          width: 32,
+                                          height: 32)),
+                                  Visibility(
+                                      visible:
+                                          widget.statuses[i].reblog != null,
+                                      child: Positioned(
+                                          left: 16,
+                                          top: 18,
+                                          child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                              child: Image(
+                                                  image: NetworkImage(widget
+                                                      .statuses[i]
+                                                      .account
+                                                      .avatar),
+                                                  width: 18,
+                                                  height: 18)))),
+                                ]),
+                                const SizedBox(width: 5),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Visibility(
+                                        visible:
+                                            widget.statuses[i].reblog != null,
+                                        child: Text(
+                                          widget.statuses[i].account
+                                                      .displayName !=
+                                                  ""
+                                              ? "${widget.statuses[i].account.displayName} boosted"
+                                              : "${widget.statuses[i].account.username} boosted",
+                                          style: const TextStyle(
+                                              color: Colors.grey),
+                                          textAlign: TextAlign.left,
+                                        )),
+                                    Text(
+                                        widget.statuses[i].reblog != null
+                                            ? widget.statuses[i].reblog!.account
+                                                        .displayName !=
+                                                    ""
+                                                ? widget.statuses[i].reblog!
+                                                    .account.displayName
+                                                : widget.statuses[i].reblog!
+                                                    .account.username
+                                            : widget.statuses[i].account
+                                                        .displayName !=
+                                                    ""
+                                                ? widget.statuses[i].account
+                                                    .displayName
+                                                : widget.statuses[i].account
+                                                    .username,
+                                        textAlign: TextAlign.left,
+                                        overflow: TextOverflow.clip,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                    Text(
+                                      widget.statuses[i].reblog != null
+                                          ? "@${widget.statuses[i].reblog!.account.username}"
+                                          : "@${widget.statuses[i].account.username}",
                                       style:
                                           const TextStyle(color: Colors.grey),
                                       textAlign: TextAlign.left,
-                                    )),
-                                Text(
-                                    widget.statuses[i].reblog != null
-                                        ? widget.statuses[i].reblog!.account
-                                                    .displayName !=
-                                                ""
-                                            ? widget.statuses[i].reblog!.account
-                                                .displayName
-                                            : widget.statuses[i].reblog!.account
-                                                .username
-                                        : widget.statuses[i].account
-                                                    .displayName !=
-                                                ""
-                                            ? widget
-                                                .statuses[i].account.displayName
-                                            : widget
-                                                .statuses[i].account.username,
-                                    textAlign: TextAlign.left,
-                                    overflow: TextOverflow.clip,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold)),
-                                Text(
-                                  widget.statuses[i].reblog != null
-                                      ? "@${widget.statuses[i].reblog!.account.username}"
-                                      : "@${widget.statuses[i].account.username}",
-                                  style: const TextStyle(color: Colors.grey),
-                                  textAlign: TextAlign.left,
+                                    )
+                                  ],
                                 )
                               ],
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Center(
-                            child: HtmlWidget(widget.statuses[i].reblog != null
-                                ? widget.statuses[i].reblog!.spoilerText != ""
-                                    ? widget.statuses[i].reblog!.spoilerText
-                                    : widget.statuses[i].reblog!.content
-                                : widget.statuses[i].spoilerText != ""
-                                    ? widget.statuses[i].spoilerText
-                                    : widget.statuses[i].content)),
-                        Visibility(
-                            visible: (widget.statuses[i].spoilerText != "" ||
-                                    (widget.statuses[i].reblog != null &&
-                                        widget.statuses[i].reblog!
-                                                .spoilerText !=
-                                            "")) &&
-                                !revealedStatuses
+                            ),
+                            const SizedBox(height: 10),
+                            Center(
+                                child: HtmlWidget(widget.statuses[i].reblog !=
+                                        null
+                                    ? widget.statuses[i].reblog!.spoilerText !=
+                                            ""
+                                        ? widget.statuses[i].reblog!.spoilerText
+                                        : widget.statuses[i].reblog!.content
+                                    : widget.statuses[i].spoilerText != ""
+                                        ? widget.statuses[i].spoilerText
+                                        : widget.statuses[i].content)),
+                            Visibility(
+                                visible: (widget.statuses[i].spoilerText !=
+                                            "" ||
+                                        (widget.statuses[i].reblog != null &&
+                                            widget.statuses[i].reblog!
+                                                    .spoilerText !=
+                                                "")) &&
+                                    !revealedStatuses
+                                        .contains(widget.statuses[i].id),
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        revealedStatuses
+                                            .add(widget.statuses[i].id);
+                                      });
+                                    },
+                                    child: const Text("Show More"))),
+                            Visibility(
+                                visible: revealedStatuses
                                     .contains(widget.statuses[i].id),
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    revealedStatuses.add(widget.statuses[i].id);
-                                  });
-                                },
-                                child: const Text("Show More"))),
-                        Visibility(
-                            visible: revealedStatuses
-                                .contains(widget.statuses[i].id),
-                            child: HtmlWidget(widget.statuses[i].reblog != null
-                                ? widget.statuses[i].reblog!.content
-                                : widget.statuses[i].content)),
-                        Visibility(
-                            visible: (widget.statuses[i].spoilerText == "" ||
-                                    (widget.statuses[i].reblog != null &&
-                                        widget.statuses[i].reblog!
-                                                .spoilerText ==
-                                            "")) &&
-                                !revealedStatuses
-                                    .contains(widget.statuses[i].id),
-                            child: Column(children: [
-                              for (var media in widget.statuses[i].reblog != null
-                                  ? widget.statuses[i].reblog!.mediaAttachments
-                                  : widget.statuses[i].mediaAttachments)
-                                Padding(
-                                    padding:
-                                        const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                                    child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(5),
-                                        child: media.type ==
-                                                api.MediaAttachmentType.audio
-                                            ? SiriWaveform.ios9(
-                                                controller:
-                                                    IOS9SiriWaveformController(
-                                                        amplitude: 0),
-                                                options: IOS9SiriWaveformOptions(
-                                                    height: 40,
-                                                    width: MediaQuery.of(context)
-                                                            .size
-                                                            .width -
-                                                        20))
-                                            : Image(
-                                                image: NetworkImage(media.previewUrl ?? ''),
-                                                width: MediaQuery.of(context).size.width - 20)))
-                            ]))
-                      ]))),
+                                child: HtmlWidget(
+                                    widget.statuses[i].reblog != null
+                                        ? widget.statuses[i].reblog!.content
+                                        : widget.statuses[i].content)),
+                            Visibility(
+                                visible: (widget.statuses[i].spoilerText ==
+                                            "" ||
+                                        (widget.statuses[i].reblog != null &&
+                                            widget.statuses[i].reblog!
+                                                    .spoilerText ==
+                                                "")) &&
+                                    !revealedStatuses
+                                        .contains(widget.statuses[i].id),
+                                child: Column(children: [
+                                  for (var media
+                                      in widget.statuses[i].reblog != null
+                                          ? widget.statuses[i].reblog!
+                                              .mediaAttachments
+                                          : widget.statuses[i].mediaAttachments)
+                                    Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 5, 0, 0),
+                                        child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                            child: media.type == api.MediaAttachmentType.audio
+                                                ? SiriWaveform.ios9(
+                                                    controller:
+                                                        IOS9SiriWaveformController(
+                                                            amplitude: 0),
+                                                    options: IOS9SiriWaveformOptions(
+                                                        height: 40,
+                                                        width:
+                                                            MediaQuery.of(context).size.width - 20))
+                                                : const SizedBox()))
+                                ]))
+                          ])))
+                  : const SizedBox(),
               itemCount: widget.statuses.length,
             ),
     ));
